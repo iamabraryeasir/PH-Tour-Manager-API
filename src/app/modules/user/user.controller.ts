38f9a1a657b5057fcc3fd49a77d8518b6ea3c481
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-07-10 02:23:15 +0600

    created catchAsync fn for reducing code redundancy

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * Node Modules
  */
@@ -8,12 +9,13 @@ import httpStatusCodes from 'http-status-codes';
  * Local Modules
  */
 import { UserServices } from './user.service';
+import { catchAsync } from '../../utils/catchAsync';
 
 /**
  * User Controllers
  */
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
 
     res.status(httpStatusCodes.CREATED).json({
@@ -21,15 +23,11 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       message: 'User created successfully',
       data: user,
     });
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-    next(error);
   }
-};
+);
 
-const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const users = await UserServices.getAllUsers();
 
     res.status(httpStatusCodes.OK).json({
@@ -37,10 +35,8 @@ const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
       message: 'Successfully fetched all users',
       data: users,
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 export const UserController = {
   createUser,
